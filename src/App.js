fb32dec38483b54fb4df3221265198dcbb080ccd
bryanparmelee <bryan.parmelee@gmail.com>
2022-07-24 16:58:13 -0400

    Add API call

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { useEffect } from "react";
 
 
 
 function App() {
+  useEffect(() => {
+    function getTrivia() {
+      fetch('https://opentdb.com/api.php?amount=10')
+      .then(res => res.json())
+      .then(data => console.log(data.results))
+    }
+    getTrivia()
+  }, [])
+
+
+
   return (
     <div className="App">
      <h1>Hello world</h1>
